3c4d2f4597801657a6155c95dd8f000b6e3d3e46
john1199 <jhon091199@gmail.com>
2021-10-23 21:19:37 -0500

    se añaden formularios y tabla productos

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Productos = () => {
-  return <div>Productos</div>;
+  return (
+    <>
+      <h2 className="text-center my-5">
+        <table className="table table-striped">
+          <thead className="bg-primary table-dark">
+            <tr>
+              <th scope="col">Nombre</th>
+              <th scope="col">Precio</th>
+              <th scope="col">Acciones</th>
+            </tr>
+          </thead>
+        </table>
+      </h2>
+    </>
+  );
 };
 
 export default Productos;
